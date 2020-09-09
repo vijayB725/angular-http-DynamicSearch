0e61eabfc1c595c0c60d990b9a2d43478a96bf82
vijayB725 <kjvj725@gmail.com>
2020-09-09 11:47:52 +0530

    3 files modified, 1 added

--- src/app/data-list/data-list.component.ts
+++ src/app/data-list/data-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpDataServiceService } from "../../http-data-service.service";
 import { album } from "../../app.album.model";
+import { picture } from "../app.picture.model";
 
 @Component({
   selector: "app-data-list",
@@ -8,11 +9,14 @@ import { album } from "../../app.album.model";
   styleUrls: ["./data-list.component.css"]
 })
 export class DataListComponent implements OnInit {
+
   private albumList: album[];
+  private albumSelected: number;
+  private pictureList: picture[];
 
   constructor(private _dataService: HttpDataServiceService) {}
 
   ngOnInit() {
-    this._dataService.getData().subscribe(data => (this.albumList = data));
+    this._dataService.getAlbums().subscribe(data => (this.albumList = data));
   }
 }
--- src/http-data-service.service.ts
+++ src/http-data-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -9,10 +9,18 @@ import {Observable} from 'rxjs';
 export class HttpDataServiceService {
 
   private albumUrl = "https://jsonplaceholder.typicode.com/albums";
+  private pictureUrl = "https://jsonplaceholder.typicode.com/photos";
+
   constructor(private _http: HttpClient) { }
 
-  getData(): Observable<any>{
+  getAlbums(): Observable<any>{
     return this._http.get(this.albumUrl);
+  }
+
+  getPicturesByAlbumId(_albumId: string): Observable<any>{
+      let  params1 = new HttpParams().set('albumId', _albumId);
+
+    return this._http.get(this.pictureUrl, {params:params1});
   }
 
 }
